1765493a67c9f4d17412482f103fb9cc9bc87071
Nadjet Tobbal <nadjetobbal@gmail.com>
2020-10-01 09:50:59 +0200

    Initial commit

---
+++ Assets/Scripts/Cylinder.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cylinder : MonoBehaviour
+{
+    public Material material;
+
+    [Range(1, 100)]
+    public int radius = 2;
+    [Range(1, 100)]
+    public int height = 5;
+    [Range(5, 50)]
+    public int meridians = 5;
+
+
+    private int old_radius = 0;
+    private int old_height = 0;
+    private int old_meridians = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.AddComponent<MeshFilter>();
+        gameObject.AddComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (old_radius !=  radius || old_height != height || old_meridians != meridians)
+        {
+            //sommets
+            Vector3[] vertices = new Vector3[(meridians * 2) + 2];
+            //triangles (qui créent les cercles (-h/2, h/2))
+            int[] triangles = new int[6 * meridians * 2]; 
+
+                for (int i = 0; i < meridians; i++)
+                {
+                    // calcule la valeur de θi= 2π i/m
+                    float theta = 2 * Mathf.PI * i / meridians;
+
+                    //crée les sommets des méridien 
+                    vertices[i * 2] = new Vector3(radius * Mathf.Cos(theta), -height / 2, radius * Mathf.Sin(theta));
+                    vertices[i * 2 + 1] = new Vector3(radius * Mathf.Cos(theta), height / 2, radius * Mathf.Sin(theta));
+
+                    //calcule l'index du prochain mer
+                    int nexti = (i < meridians - 1) ? i + 1 : 0;
+
+                    //assigne des sommets 
+                    triangles[i * 6] = i * 2;
+                    triangles[i * 6 + 1] = i * 2 + 1;
+                    triangles[i * 6 + 2] = nexti * 2;
+
+                    triangles[i * 6 + 3] = i * 2 + 1;
+                    triangles[i * 6 + 4] = nexti * 2 + 1;
+                    triangles[i * 6 + 5] = nexti * 2;
+
+                }
+
+            vertices[meridians * 2] = new Vector3(0, -height / 2, 0);
+            vertices[meridians * 2 + 1] = new Vector3(0, height / 2, 0);
+
+            int offset = meridians * 6;
+
+                for (int i = 0; i < meridians; ++i)
+                {
+                    //calcule l'index du prochain mer
+                    int ni = (i < meridians - 1) ? i + 1 : 0; 
+
+                    triangles[offset + i * 6] = meridians * 2;
+                    triangles[offset + i * 6 + 1] = i * 2;
+                    triangles[offset + i * 6 + 2] = ni * 2;
+
+                    triangles[offset + i * 6 + 3] = meridians * 2 + 1;
+                    triangles[offset + i * 6 + 4] = ni * 2 + 1;
+                    triangles[offset + i * 6 + 5] = i * 2 + 1;
+                }
+
+                Mesh mesh = new Mesh();
+
+                mesh.vertices = vertices;
+                mesh.triangles = triangles;
+
+                gameObject.GetComponent<MeshFilter>().mesh = mesh;
+                gameObject.GetComponent<MeshRenderer>().material = material;
+        }
+    }
+}
---
+++ Assets/Scripts/Triangle_Plane.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triangle_Plane : MonoBehaviour
+{
+    public Material material;
+
+    [Range(1, 10)]
+    public int planeHeight = 1;
+    [Range(1, 10)]
+    public int planeWidth = 1;
+
+    private int old_planeHeight = 0;
+    private int old_planeWidth = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.AddComponent<MeshFilter>();
+        gameObject.AddComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(old_planeHeight != planeHeight || old_planeWidth != planeWidth)
+        {
+            //position des sommets
+            Vector3[] vertices = new Vector3[4 * planeHeight * planeWidth];
+            //tableau du n° de triangles
+            int[] triangles = new int[6 * planeWidth * planeHeight];
+
+            for(int x = 0; x < planeHeight; x++)
+            {
+                for(int y = 0; y < planeWidth; y++)
+                {
+                    int i = (x + y * planeHeight) * 4;
+                    vertices[i] = new Vector3(x, y, 0);
+                    vertices[i + 1] = new Vector3(x + 1, y, 0);
+                    vertices[i + 2] = new Vector3(x, y + 1, 0);
+                    vertices[i + 3] = new Vector3(x + 1, y + 1, 0);
+
+                    int j = (x + y * planeHeight) * 6;
+                    triangles[j] = i;
+                    triangles[j + 1] = i + 1;
+                    triangles[j + 2] = i + 2;
+                    triangles[j + 3] = i + 1;
+                    triangles[j + 4] = i + 3;
+                    triangles[j + 5] = i + 2;
+                }
+            }
+
+            //vertices[0] = new Vector3(0,0,0); 
+            //vertices[1] = new Vector3(1,0,0); 
+            //vertices[2] = new Vector3(0,1,0); 
+
+            //triangles[0] = 0;
+            //triangles[1] = 1;
+            //triangles[2] = 2;
+
+            Mesh mesh = new Mesh();
+
+            mesh.vertices = vertices;
+            mesh.triangles = triangles;
+
+            gameObject.GetComponent<MeshFilter>().mesh = mesh;
+            gameObject.GetComponent<MeshRenderer>().material = material;
+
+        }
+
+    }
+}
